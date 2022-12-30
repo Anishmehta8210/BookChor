7f22afe74032135f3426eb526f71eeaffb5788eb
Anishmehta8210 <anishmehta7782@gmail.com>
2022-12-30 18:27:53 +0530

    cart

--- ebook/book/models.py
+++ ebook/book/models.py
@@ -32,6 +32,18 @@ class OrderItem(models.Model):
 
      def __str__(self):
          return self.user.username
+
+     def getPrice(self):
+        return self.item.price * self.qty
+
+     def getDiscountPrice(self):
+        return self.item.discount_price * self.qty
+
+     def getDiscountPercent(self):
+        discount_amount = self.item.price - self.item.discount_price
+        percent = discount_amount * 100/500
+        return percent
+        
      
 
 ADDRESS_TYPE = (
@@ -87,6 +99,30 @@ class Order (models.Model):
     coupon = models.ForeignKey(Coupon,on_delete=models.SET_NULL,null=True,blank=True)
     being_delivered = models.BooleanField(default=False)
     recieved = models.BooleanField(default=False)
+
+
+    def getTotalPrice(self):
+        total = 0
+        for oi in self.items.all():
+            total += oi.getPrice()
+        return total
+    
+    def getTotalAfterDiscountPrice(self):
+        total = 0
+        for oi in self.items.all():
+            total += oi.getDiscountPrice()
+        if self.coupon:
+            total -= self.coupon.amount
+        return total
+    
+    def getTotalDiscountPrice(self):
+        return self.getTotalPrice() - self.getTotalAfterDiscountPrice()
+
+    def getTax(self):
+        return self.getTotalAfterDiscountPrice() * 0.18
+
+    def getPayableAmount(self):
+        return self.getTotalAfterDiscountPrice() + self.getTax()
 
     def __str__(self):
         return self.user.username
